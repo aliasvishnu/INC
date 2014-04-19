509e4a4abcabb7f6ffc2256eb5605bd6ae2e2de0
aliasvishnu <aliasvishnu@gmail.com>
2014-04-20 00:04:04 +0530

    Fully working Minimax based TicTacToe

--- INC.cpp
+++ INC.cpp
@@ -1,118 +1,240 @@
 #include <iostream>
 #include <stdio.h>
 #include <vector>
+#include <algorithm>
+#include <string.h>
+#include <stdlib.h>
 
 using namespace std;
+
+// O for computer
+// X for player
+
+struct threeInt{
+	int first;
+	int second;
+	int third;
+};
+
+struct twoInt{
+	int first;
+	int second;
+};
 
 class board{
 private:
 	char brd[3][3];
-	int score;
 	char turn[10];
 	int depth;
-	vector<pair<int, int> > availableMoves;
-	vector<pair<int, int> > availableScores;
+	int mainBoard;
+	struct twoInt choice;
 public:
-	board(char player[]){
+	char getValue(int locX, int locY){
+		return brd[locX][locY];
+	}
+	void setValue(char sym, int locX, int locY){
+		brd[locX][locY] = sym;
+	}
+	void putValue(int locX, int locY){
+		cout << " " << brd[locX][locY] << " ";
+	}
+	char* getTurn(){
+		return turn;
+	}
+	void togglePlayer(){
+		if(!strcmp(getTurn(), "player")) strcpy(turn, "computer");
+		else strcpy(turn, "player");
+	}
+	board(){
 		for(int i = 0; i < 3; i++){
 			for (int j = 0; j < 3; ++j){
-				this.setValue(' - ', i, j);
+				setValue('-', i, j);
 			}
 		}
-		strcpy(this.turn, "player");
+		choice.first = 0; choice.second = 0;
+		strcpy(turn, "computer");
 		depth = 0;
+		mainBoard = 1;
 	}
-
 	board(board parent, int locX, int locY){
 		for(int i = 0; i < 3; i++){
 			for (int j = 0; j < 3; ++j){
-				this.setValue(parent.getValue(i, j), i, j);
+				setValue(parent.getValue(i, j), i, j);
 			}
 		}	
-		this.depth += 1;
-		this.togglePlayer();
-		if(strcmp == "player"){
-			this.brd[locX][locY] = 'x';
+		depth = parent.depth + 1;		
+		strcpy(turn, parent.getTurn());
+		togglePlayer();
+		mainBoard = 0;
+
+		if(!strcmp(turn, "player")){
+			setValue('X', locX, locY);
 		}else{
-			this.brd[locX][locY] = '0';
+			setValue('O', locX, locY);
 		}
 	}
-	
-	void setValue(char sym, int locX, int locY){
-		this.brd[locX][locY] = sym;
-	}
-
-	void putValue(int locX, int locY){
-		cout << " " << this.brd[locX][locY] << " ";
-	}
-
-	void togglePlayer(char turnParent[]){
-		if(!strcmp(turnParent, "computer")) strcpy(this.turn, "player");
-		if(!strcmp(turnParent, "player")) strcpy(this.turn, "computer");
-	}
-
 	void displayBoard(){
+		cout << "\n";
 		for(int i = 0; i < 3; i++){
 			for (int j = 0; j < 3; ++j){
-				this.putValue(' - ', i, j);
+				putValue(i, j);
 			}
+			cout << "\n";
 		}
 	}
-
-	char getValue(int locX, int locY){
-		return this.brd[locX][locY];
+	void computerMove(){
+		if(getValue(1, 1) == '-'){
+			setValue('O', 1, 1)	;
+		}else{
+			minimax();
+		    setValue('O', choice.first, choice.second);
+		}
+	    depth += 1;		
+		displayBoard();
+		if(winner() == 1){
+			cout << "You lost!" << "\n";
+			exit(0);
+		}
+		if(over()){
+			cout << "Draw!\n";
+			exit(0);
+		}
+		playerMove();
 	}
 
-	int winner(){
-		if(brd[0][0] == brd [1][1] && brd [1][1] == brd[2][2]) return 1;
-		if(brd[0][0] == brd [0][1] && brd [0][1] == brd[0][2]) return 1;
-		if(brd[1][0] == brd [1][1] && brd [1][1] == brd[1][2]) return 1;
-		if(brd[2][0] == brd [2][1] && brd [2][1] == brd[2][2]) return 1;
-		if(brd[0][0] == brd [1][0] && brd [1][0] == brd[2][0]) return 1;
-		if(brd[0][1] == brd [1][1] && brd [1][1] == brd[2][1]) return 1;
-		if(brd[0][2] == brd [1][2] && brd [1][2] == brd[2][2]) return 1;
-		if(brd[2][0] == brd [1][1] && brd [1][1] == brd[0][2]) return 1;
-		return 0;
-	}
+	void playerMove(){
+		int a;
+		cout << "\nWhere would you like to place?";
+		cin >> a;
 
-	void calculateAvailableMoves(){
-		for(int i = 0; i < 3 ; i++){
-			for(int j = 0; j < 3 ; j++){
-				if(this.getValue(i, j) == '-') this.availableMoves.push_back(pair<int, int>(i, j));
+		int locX, locY;
+		switch(a){
+			case 1 : locX = 0; locY = 0; break;
+			case 2 : locX = 0; locY = 1; break;
+			case 3 : locX = 0; locY = 2; break;
+			case 4 : locX = 1; locY = 0; break;
+			case 5 : locX = 1; locY = 1; break;
+			case 6 : locX = 1; locY = 2; break;
+			case 7 : locX = 2; locY = 0; break;
+			case 8 : locX = 2; locY = 1; break;
+			case 9 : locX = 2; locY = 2; break;
+		}
+
+		if(getValue(locX, locY) == '-'){
+			setValue('X', locX, locY);
+			depth += 1;
+			displayBoard();
+			if(winner() == 1){
+				cout << "You have won\n";
+				exit(0);
 			}
+			if(over()){
+				cout << "Draw!\n";
+				exit(0);
+			}
+			computerMove();
+		}else {
+			cout << "\nThat's taken dear!";
+			playerMove();
 		}
 	}
-
 	int score(){
-		if(strcmp(this.turn, "player"))	return 10 - depth;
-		else return depth;
+		if(strcmp(turn, "player") && winner()){ //Meaning computer won
+			return (20 - depth);
+		}	
+		else if(strcmp(turn, "computer") && winner()) {
+			return (depth - 20) ;
+		}
+		else{ 
+			return 0;
+		}
 	}
+	// Says if the game got over - Take care when to call
+	int winner(){
+		int game = 0;		// Nobody has won yet
 
-	int minimax(this){
-		// Check if the game is over or not
-		if(this.winner()) return this.score();
+		if(brd[0][0] == brd [1][1] && brd [1][1] == brd[2][2] && brd[2][2] != '-') game = 1;
+		if(brd[0][0] == brd [0][1] && brd [0][1] == brd[0][2] && brd[0][2] != '-') game = 1;
+		if(brd[1][0] == brd [1][1] && brd [1][1] == brd[1][2] && brd[1][2] != '-') game = 1;
+		if(brd[2][0] == brd [2][1] && brd [2][1] == brd[2][2] && brd[2][2] != '-') game = 1;
+		if(brd[0][0] == brd [1][0] && brd [1][0] == brd[2][0] && brd[2][0] != '-') game = 1;
+		if(brd[0][1] == brd [1][1] && brd [1][1] == brd[2][1] && brd[2][1] != '-') game = 1;
+		if(brd[0][2] == brd [1][2] && brd [1][2] == brd[2][2] && brd[2][2] != '-') game = 1;
+		if(brd[2][0] == brd [1][1] && brd [1][1] == brd[0][2] && brd[0][2] != '-') game = 1;
 
-		// Calculate all possible moves in the board
-		calculateAvailableMoves(); 
-
-		// For each move, find out the score
-		for (vector<pair<int, int> >::iterator i = this.availableMoves.begin(); i != this.availableMoves.end(); ++i){
-			board newGamePath = new board(this, (*i)->first, (*i)->second);
-			this.availableScores.push_back(newGamePath.minimax());
+		return game;
+	}
+	bool over(){
+		int flag = 1;
+		for(int i = 0; i < 3; i++){
+			for (int j = 0; j < 3; ++j){
+				if(getValue(i, j) == '-') flag = 0;
+			}
 		}
 
-		if(strcmp(this.turn, "player") == ){
-			// Meaning next move belongs to computer	
-		} 
-
+		int ifWin = winner();
+		// Condition says if someone won or if the game is draw
+		if(ifWin || (!ifWin && flag)) return 1;
+		else return 0;
 	}
 
+	int minimax(){
+		if(over()){
+			return score();
+		}
 
+		struct twoInt moves[10], temp;
+		int scores[10], count = 0;
 
-	~board();
-	
+		// Collect all legal moves into the moves array
+		for(int i = 0; i < 3; i++){
+			for (int j = 0; j < 3; ++j){
+				if(getValue(i, j) == '-'){
+					temp.first = i; temp.second = j;
+					moves[count++] = temp;
+				}
+			}
+		}
+		
+		if(!strcmp(turn, "player")){
+			int bestValue = -32000;
+			for(int i = 0; i < count; i++){
+				board newGamePath(*this, moves[i].first, moves[i].second);
+				int val = newGamePath.minimax();
+				if(bestValue < val){
+					choice.first = moves[i].first;
+					choice.second = moves[i].second;
+					bestValue = val;
+				}
+			}
+			return bestValue;
+		}else{
+			int bestValue = 32000;
+			for(int i = 0; i < count; i++){
+				board newGamePath(*this, moves[i].first, moves[i].second);
+				int val = newGamePath.minimax();
+				if(bestValue > val){
+					choice.first = moves[i].first;
+					choice.second = moves[i].second;
+					bestValue = val;
+				}
+			}
+			return bestValue;
+		}
+	}
 };
 
 int main(){
-	
+	cout << "Welcome to my TIC TAC TOE\n";
+	int start = 0;
+	cout << "Do you wanna start?";
+	cin >> start;
+
+	board nemesis;
+	if(start == 1){
+			nemesis.playerMove();
+	}else{
+		nemesis.computerMove();
+	}
+return 0;
 }
